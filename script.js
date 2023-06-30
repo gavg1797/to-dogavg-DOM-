09557de26c0c70e4d986213c29574f29bdf2af8c
Giancarlo Villalobos <gavg1797@outlook.com>
2023-06-29 21:08:19 -0600

    clase04y05 completada, curso finalizado

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-( () => {
+import checkComplete from "./components/checkcomplete.js";
+import deleteIcon from "./components/deleteIcon.js";
 
 const btn = document.querySelector("[data-form-btn]")
 
@@ -21,31 +22,21 @@ taskContent.appendChild(titleTask);
 const content = `
     <span class="task">${value}</span>
 
-    <i class="fas fa-trash-alt trashIcon icon"></i>
-    </li>`
+    <i class="fas fa-trash-alt trashIcon icon"></i></li>`
 
 // task.innerHTML = content;
     task.appendChild(taskContent);
+    task.appendChild(deleteIcon());
     list.appendChild(task);
 }; 
+
 
 // Arrow Functions o Funciones flecha o funciones anonimas.
 btn.addEventListener("click",createTask);
 
-const checkComplete = () => {
-    const i = document.createElement("i")
-    i.classList.add("far","fa-check-square","icon");
-    i.addEventListener("click", completeTask)
-    return i
-}
-// Immediately invoked function expression IIFE
-const completeTask = (event) => {
-    const element = event.target
-    element.classList.toggle('fas')
-    element.classList.toggle('completeIcon')
-    element.classList.toggle('far');
-}
-    
-})();
+
+   
+
+
 
 
